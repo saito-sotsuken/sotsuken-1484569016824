73b2f437c0737cff7c041da4152391bf493c7560
saito-sotsuken <anctsaito.sotsuken@gmail.com>
2017-01-21 23:37:37 +0900

    Add files via upload

---
+++ image.php
@@ -0,0 +1,49 @@
+﻿<html>
+	<head>
+		<title>試作品</title>
+	</head>
+
+	<body>
+	<?php
+	$trans1 = 0;
+	$trans2 = 0;
+	if( isset( $_GET['url'] ) ){
+		$apikey = 'dccd4a5837cfbb770c8ac3956fa7fb72258d954c';
+		$url = $_GET['url'];
+		$alchemyurl = 'http://access.alchemyapi.com/calls/url/';
+	?>
+	<img src='<?php echo $url; ?>'/>
+	<p/>
+
+	<?php
+	$apiurl1 = $alchemyurl . 'URLGetRankedImageKeywords?apikey=' . $apikey . '&outputMode=json&url=' . urlencode( $url );
+	$text1 = file_get_contents( $apiurl1 );
+	$json1 = json_decode( $text1 );
+
+	$trans1 = $json1->totalTransactions;
+	$imageKeywords = $json1->imageKeywords;
+	if( count( $imageKeywords ) ){
+	?>
+	<h2>Keywords</h2>
+	<table border='1'>
+    <tr><th>text</th><th>score</th></tr>
+	<?php
+    for( $i = 0; $i < count( $imageKeywords ); $i ++ ){
+      $imageKeyword = $imageKeywords[$i];
+      $text = $imageKeyword->text;
+      $score = $imageKeyword->score;
+	?>
+    <tr><td><?php echo $text; ?></td><td><?php echo $score; ?></td></tr>
+<?php
+    }
+?>
+  </table>
+  <p/>
+<?php
+      }
+    }
+?>
+
+	</body>
+</html>
+
